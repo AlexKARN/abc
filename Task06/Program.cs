216a14c86bb99909e3d72468cd839706fbbefb72
AlexKARN <regidex@hotmail.com>
2023-03-22 17:30:19 +0300

    added program code Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -2,3 +2,15 @@
 // является ли число чётным (делится ли оно на два без остатка).
 
 Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0) Console.WriteLine("Является чётным");
+
+else 
+{
+    Console.WriteLine("Не является чётным");
+};
+
+Console.WriteLine();
+
