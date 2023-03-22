261accf7ebad363bc063694d4264964ca15114a3
AlexKARN <regidex@hotmail.com>
2023-03-22 08:16:49 +0300

    created program code Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,7 +4,7 @@
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());max
-Console.WriteLine("Введите второе число: ");
+Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
 int maxNumber;
