6be2ca6b5d74f398cf89c23fb9510903195f6013
AlexKARN <regidex@hotmail.com>
2023-03-22 16:23:44 +0300

    added program code to Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,3 +2,9 @@
 // а на выходе показывает все чётные числа от 1 до N.
 
 Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i=2; i  N; i +=2)
+    Console.Write(i);
+
