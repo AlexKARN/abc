846d952ff373257a45a1cb390a7d832fd33930dd
AlexKARN <regidex@hotmail.com>
2023-03-22 08:41:26 +0300

    created program code Task06

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход три числа 
 // и выдает максимальное из этих чисел.
- Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -10,11 +10,11 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 int maxNumber;
 
 if (number1 > number2)
-maxNumber = number1;
+    maxNumber = number1;
 else
-maxNumber = number2;
+    maxNumber = number2;
 
 if (number3 > maxNumber)
-maxNumber = number3;
+    maxNumber = number3;
 
 Console.WriteLine($"Максимальным является число {maxNumber}");
--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает число
+// и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
 Console.WriteLine("Hello, Task06 !");
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+Console.WriteLine($"Число {number} является чётным");
+else
+Console.WriteLine($"Число {number} НЕ является чётным");
