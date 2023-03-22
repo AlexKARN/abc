1f7feb0e53fe8f0a596f2ce28a9b4a1992dc71df
AlexKARN <regidex@hotmail.com>
2023-03-22 15:18:47 +0300

    changed program codes Task02 and Task04

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿// Задача 02:  Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+ 
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
