5d73f264d028a6a98901a71148d34f4dd777869e
AlexKARN <regidex@hotmail.com>
2023-03-23 17:23:08 +0300

    changed code Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, My Task00!");
+Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int absnum = Math.Abs(number);
+
+if (absnum > 99 && absnum < 1000) 
+    {
+        int tempnum = absnum/10;
+        int secondNumber = tempnum % 10;
+        Console.Clear();
+        Console.WriteLine($"Введённое число: {number}");
+        Console.WriteLine($"Вторая цифра введенного числа: {secondNumber}");
+    }
+    
+    
+else
+    { 
+       Console.WriteLine("Введено ненадлежащее число.");
+    }
