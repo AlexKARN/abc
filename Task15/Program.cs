d6487f59e5384b5a3590a50596516622c306e8ec
AlexKARN <regidex@hotmail.com>
2023-03-23 19:41:10 +0300

    corrected code Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -7,6 +7,6 @@
 
 Console.Clear();
 Console.WriteLine("Введите цифру, обозначающую день недели (от 1 до 7):");
-int weekDay = Convert.ToInt32(Console.Read());
+int weekDay = Convert.ToInt32(Console.ReadLine());
 if (weekDay == 6 || weekDay == 7) Console.WriteLine($"День {weekDay} является выходным.");
 else Console.WriteLine($"День {weekDay} не является выходным.");
