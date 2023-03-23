1060e2a6378b3e5f6574ce1b971e36ae3ed80d6a
AlexKARN <regidex@hotmail.com>
2023-03-23 14:12:30 +0300

    edited code Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
-//и на выходе показывает вторую цифру этого числа.
+// и на выходе показывает вторую цифру этого числа.
+
 Console.Clear();
 Console.WriteLine("Введите трехзначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -12,10 +13,10 @@ if (absnum > 99 && absnum < 1000)
         Console.Clear();
         Console.WriteLine($"Введённое число: {number}");
         Console.WriteLine($"Вторая цифра введенного числа: {secondNumber}");
-    
-    
+    }
+        
 else
     { 
        Console.WriteLine("Введено ненадлежащее число.");
-    }
+    };
 
