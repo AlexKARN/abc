7b702cac8d066c53f27c8e9cc576cb49d088e907
AlexKARN <regidex@hotmail.com>
2023-03-25 01:08:50 +0300

    added code Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдает таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintQubesRow(number);
+
+
+void PrintQubesRow(int num)
+{
+Console.WriteLine("Число  Куб числа");
+for(int i = 1; i <= num; i++)
+{
+    int qube = i*i*i;
+    Console.WriteLine($"{i,3} {qube, 9}");
+}
+return;
+}
