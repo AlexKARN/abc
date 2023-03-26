6e19d37af2d128f00175061d44a9ade968ac6383
AlexKARN <regidex@hotmail.com>
2023-03-26 23:23:24 +0300

    added Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в произвольном порядке.
+// [1,0,1,1,0,1,0,0]
+Console.Clear();
+Console.Write("Введите размер массива:");
+int arraySize = Convert.ToInt32(Console.Read());
+
+if (arraySize <= 0)
+{
+    Console.WriteLine("Введено неверное значение!");
+    return;
+}
+ 
+ int[] arr = CreateArray(arraySize);   // вызов метода
+
+PrintArray(arr);                    // вызов void-метода
+
+
+int[] CreateArray(int arrayLength)
+{
+    int[] newArray = new int[arrayLength];
+    Random random = new Random();
+
+    for(int i = 0; i < arrayLength; i++)
+    {
+        newArray[i] = random.Next(0,2);
+    }
+    return newArray;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
