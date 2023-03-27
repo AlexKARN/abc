2801de3843d18c8a3daa39eb7d3ecc817eac0ab6
AlexKARN <regidex@hotmail.com>
2023-03-28 00:25:57 +0300

    added code Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив, заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+if (arraySize <= 0)
+{
+    Console.WriteLine("Введено некорректное значение!");
+    return;
+}
+
+int[] arr = CreateArray(arraySize);     // вызов метода создания массива
+
+PrintArray(arr);                        // вызов метода печати массива
+
+int numberOfEvens = NumberOfEvens(arr);                // вызов метода подсчета чётных чисел в массиве
+
+Console.WriteLine($"Количество чётных чисел в массиве равно: {numberOfEvens}");
+
+int[] CreateArray(int arrayLength)     // задание метода CreateArray
+{
+    int[] newArray = new int[arrayLength];
+    
+    for(int i = 0; i < arrayLength; i++)
+    {
+        newArray[i] = new Random().Next(100,1000);
+    }
+    return newArray;
+} 
+
+void PrintArray(int[] array)           // задание метода вывода массива на консоль
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int NumberOfEvens(int[] arr1)                // задание метода подсчета чётных чисел в массиве 
+{
+    int result = 0;
+    for(int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i] % 2 == 0)  result++; 
+    }
+    return result;
+}
