ae6c9f69941fb92391c925b08eeec8bfb0710681
AlexKARN <regidex@hotmail.com>
2023-04-07 13:10:57 +0300

    added Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: 
+
+int n = 2;
+int m = 10;
+
+int PowerOfNumber(int num1, int num2)
+{
+    if (num2 ==00) return 1;
+    else return (num1*PowerOfNumber(num1, num2-1));
+}
+
+int p = PowerOfNumber(n,m);
+Console.WriteLine($"основание степени:{n}, показатель степени {m}, {p}");
+
